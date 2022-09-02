87c7e67e41241c76d654f2de845e17d2695888f6
Tian Yu Fan <tianyufan01@gmail.com>
2022-09-02 11:16:54 -0400

    Initial commit, successful build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc']
+master_doc = 'index'
 
 templates_path = ['_templates']
 exclude_patterns = []
